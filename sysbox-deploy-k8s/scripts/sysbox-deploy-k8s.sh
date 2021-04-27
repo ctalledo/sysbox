fd0aa8fc4fe8ba10daeb4f99d1066a45f7ea4054
Cesar Talledo <ctalledo@nestybox.com>
2021-04-27 03:36:29 +0000

    Renamed sysbox-deploy -> sysbox-deploy-k8s.
    Signed-off-by: Cesar Talledo <ctalledo@nestybox.com>

--- sysbox-deploy-k8s/scripts/sysbox-deploy-k8s.sh
+++ sysbox-deploy-k8s/scripts/sysbox-deploy-k8s.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
 #
-# Script to install or remove Sysbox on a Kubernetes node.
+# Copyright 2019-2020 Nestybox, Inc.
 #
-# Note: adapted from kata-deploy (github.com/kata-containers/packaging/tree/master/kata-deploy)
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#
+# Script to install or remove Sysbox on a Kubernetes node. The script assumes it
+# will run inside the sysbox deploy daemonset container, and that several host
+# directories are mounted onto the container. The script requires full root
+# privileges on the host (e.g., CAP_SYS_ADMIN + write access to /proc) in order
+# to install Sysbox on it.
+#
+# Note: inspired by kata-deploy (github.com/kata-containers/packaging/tree/master/kata-deploy)
 #
 
 set -o errexit
